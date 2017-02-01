8b95ba8f996c5843bb942c16017477833485c2d6
Conti <agc11d@gmail.com>
2017-02-01 17:39:24 -0500

    feat: increased blur effect

--- src/TVShader.js
+++ src/TVShader.js
@@ -1,30 +1,6 @@
 import * as THREE from 'three'
 
 const vertexShader = `
-/*
-The MIT License (MIT)
-
-Copyright (c) 2017 Andrew Conti
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 uniform float time;
 uniform sampler2D tDiffuse;
 uniform vec2 resolution;
@@ -37,32 +13,6 @@ void main() {
 }
 `
 const fragmentShader = `
-/*
-The MIT License (MIT)
-
-Copyright (c) 2017 Andrew Conti
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-Based on Justin Saunders ChromaticalShader. https://www.shadertoy.com/view/XdXXD4
-*/
-
 uniform float time;
 uniform sampler2D tDiffuse;
 uniform vec2 resolution;
@@ -78,10 +28,10 @@ void main() {
 
 	// blur amount
 	float blur = 0.0;
-	blur = (1.0 + sin(time * 6.0)) * 0.5;
-	blur *= 1.0 + sin(time * 16.0) * 0.5;
+	blur = (1.0 + sin(time * 10.0)) * 0.5;
+	blur *= 1.0 + sin(time * 25.0) * 0.5;
 	blur = pow(blur, 3.0);
-	blur *= 0.05;
+	blur *= 0.125;
 	// reduce blur towards center
 	blur *= d;
 
